4203c18ea35926a4591d4e85c5d332932c095893
Kyle Kastner <kastnerkyle@gmail.com>
2017-03-18 20:23:14 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Anlyzing samples from the paper, gotten from https://github.com/kuleshov/audio-super-res on Saturday March 18, 2017
---
+++ plot_paired_sr_examples.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import sys
+from scipy.io import wavfile
+
+fname1 = sys.argv[1]
+fs1, d1 = wavfile.read(fname1)
+
+
+fname2 = sys.argv[2]
+fs2, d2 = wavfile.read(fname2)
+
+f, axarr = plt.subplots(1, 2)
+
+nfft = 2048
+noverlap = int(.8 * nfft)
+kwargs = {"interpolation":None,
+        "noverlap":noverlap,
+        "NFFT":nfft,
+        "cmap":"gray"}
+ml = min(len(d1), len(d2))
+axarr[0].specgram(d1, **kwargs)
+axarr[0].set_title(fname1)
+axarr[1].specgram(d2, **kwargs)
+axarr[1].set_title(fname2)
+plt.show()
